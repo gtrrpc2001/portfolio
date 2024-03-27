d9506939b193de43ec5a4c24ecc6fbb85754371a
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-03-27 20:13:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Navigate, Route, Router, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from './mainHome/Home';
 
 function App() {
