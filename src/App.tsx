7be6643d2c19ad5eabe0a3208afa53c21cf1bfd1
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-03-27 20:13:13 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
 import { Navigate, Route, Router, Routes } from 'react-router-dom';
 import { Home } from './mainHome/Home';
 
 function App() {
   return (
-    // <Provider store={}>
       <Routes>
         <Route path='/' element={<Home/>}>
           
         </Route>
         <Route path='*' element={<Navigate to={'/'}/>}/>
-      </Routes>
-    // </Provider>    
+      </Routes>    
   );
 }
 
