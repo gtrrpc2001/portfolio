6beafbad6ae88b1a2c9aca821051c828d8540012
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-04-04 17:06:13 +0900

    Update Header.tsx

--- src/mainHome/header/Header.tsx
+++ src/mainHome/header/Header.tsx
@@ -1,7 +1,7 @@
 import './Header.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from 'react';
+import { useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 
@@ -9,16 +9,12 @@ export const Header = () => {
     const [menuVisible,setMenuVisible] = useState<boolean>(false)
     const width = useSelector<RootState,number>((state) => state.width)
 
-    const widthHandler = () => {        
+    const widthHandler = useCallback(()=>{
         if(Number(width) < 750)
             setMenuVisible(true)
         else
             setMenuVisible(false)
-    }
-
-    useEffect(()=>{
-        widthHandler()
-    },[width,widthHandler])    
+    },[width])   
 
     return (
         <header className='header'>
