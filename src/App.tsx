20a0225bd4a5a10996d9e54b7f12275b98048740
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-06-26 14:36:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <Provider store={store}>
       <Routes>
-        <Route path='/portfolio' element={<Home/>}>
+        <Route path='/' element={<Home/>}>
           
         </Route>
-        <Route path='*' element={<Navigate to={'/portfolio'} replace={true}/>}/>
+        <Route path='*' element={<Navigate to={'/'} replace={true}/>}/>
       </Routes>
     </Provider>    
   );
